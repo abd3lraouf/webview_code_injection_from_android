1d3e2d2944fc9f01eaa25525eb412e4640e41f17
AbdElraoufSabri <abdelraoufsabri@gmail.com>
2021-11-14 00:26:43 +0200

    manoo message

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,7 +7,7 @@
 let myButton = document.querySelector('button');
 
 myButton.onclick = function() {
-  Android.toast("Webpage is saying hi!!");
+  Android.toast("Hello ManoO!!");
 }
 
 function messageMe(message){
