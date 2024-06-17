a96c22159726cffa3f5e74ab483eb8fbc5e5e0c8
abd3lraouf <abdelraoufsabri@gmail.com>
2024-06-18 01:04:24 +0300

    :package: add new project changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,15 +1,15 @@
 /* ----------------------------
 /*  Name: webview_code_injection_from_android
-    Author: AbdElraouf Sabri
+    Author: Abdelraouf Sabri
     Version: 1.0.0
 /* -------------------------- */
 
-let myButton = document.querySelector('button');
+let myButton = document.querySelector("button");
 
-myButton.onclick = function() {
-  Android.toast("Hello ManoO!!");
-}
+myButton.onclick = function () {
+  Android.toast("Hello Great Android User!!");
+};
 
-function messageMe(message){
+function messageMe(message) {
   document.getElementById("demo").innerHTML = message;
 }
